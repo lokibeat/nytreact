9989d313187475b2489522c9472070343dfca2f3
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-09-24 18:30:24 -0500

    some small incremental progress

--- src/components/PlaceHolder/placeholder.js
+++ src/components/PlaceHolder/placeholder.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const PlaceHolder = ({ children }) => (
+const Placeholder = ({ children }) => (
   <div
     style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
     className="jumbotron"
   >
     {children}
+    <h1>{children.text}</h1>
   </div>
 );
 
-export default PlaceHolder;
+export default Placeholder;
